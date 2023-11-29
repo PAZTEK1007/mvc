5e87bc64a3ad6914eff5f432d81e92a7a350454d
Mathias Barbier <mathbarbier07@gmail.com>
2023-11-29 10:24:49 +0100

    update

--- Controller/ArticleController.php
+++ Controller/ArticleController.php
@@ -1,6 +1,5 @@
 <?php
 
-require_once 'Data\Db-Manager.php';
 
 class ArticleController
 {
--- Controller/AuthorController.php
+++ Controller/AuthorController.php
@@ -1,7 +1,5 @@
 <?php
-
-require_once 'Data\Db-Manager.php';
-
+require 'Data/DB-Manager.php';
 class AuthorController
 {
     protected $PDO;
